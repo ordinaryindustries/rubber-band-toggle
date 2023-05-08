705def7a50c067befc447e29fbbea9e4752d17ec
Ordinary Industries <132616209+ordinaryindustries@users.noreply.github.com>
2023-05-08 02:09:13 -0700

    Add documentation

--- RubberBandToggle/RubberBandToggle/ContentView.swift
+++ RubberBandToggle/RubberBandToggle/ContentView.swift
@@ -11,12 +11,20 @@ import SwiftUI
 struct CustomToggleStyle: ToggleStyle {
     @State private var offset = CGSize.zero
     
+    // Add some stickiness to the circle.
     let dragDamping: Double = 0.4
-    let offsetThreshold: CGFloat = 100.0
+    
+    // How far off the toggle the circle needs to be pulled before it snaps to the touch point.
+    let stickiness: CGFloat = 80.0
 
-    var onImage: String = "checkmark"
-    var offImage: String = "checkmark"
+    // The SF symbol to use for the toggle's on state.
+    var onImage: String = "checkmark.circle"
+    // The SF symbol to use for the toggle's on state.
+    var offImage: String = "xmark.circle"
+    
+    // The on state background color.
     var onColor: Color = Color("ToggleOn")
+    // The off state background color.
     var offColor: Color = Color("ToggleOff")
 
     func makeBody(configuration: Configuration) -> some View {
@@ -36,13 +44,17 @@ struct CustomToggleStyle: ToggleStyle {
                             .rotationEffect(.degrees(configuration.isOn ? 0 : -360))
                             .foregroundColor(.white)
                     }
+                    // The on and off points for the circle. When the toggle is tapped, the circle will be moved between these two points.
                     .offset(x: configuration.isOn ? 50 : -50)
+                    
+                    // Make the circle moveable.
                     .offset(x: offset.width, y: offset.height)
                     .gesture(
                         DragGesture()
+                            // Setup dragging the circle. If the circle is within the stickiness distance from where it started then the drag distance is manipulated to be exponentially shorter. This adds a feeling like the circle doesn't want to leave the toggle. If the circle gets past the stickeness distance then it snaps to the gesture's current position. Visually this means the circle jump to the user's finger.
                             .onChanged { gesture in
-                                if abs(offset.height) > offsetThreshold || abs(offset.width) > offsetThreshold {
-                                    withAnimation(.spring(response: 0.4, dampingFraction: 0.4)) {
+                                if abs(offset.height) > stickiness || abs(offset.width) > stickiness {
+                                    withAnimation(.spring(response: 0.3, dampingFraction: 0.4)) {
                                         offset.width = gesture.translation.width
                                         offset.height = gesture.translation.height
                                     }
@@ -51,6 +63,7 @@ struct CustomToggleStyle: ToggleStyle {
                                     offset.height = gesture.translation.height * dragDamping
                                 }
                             }
+                            // When the user lifts their finger snap the circle back to the toggle with some springiness. This also toggles the toggle state.
                             .onEnded { _ in
                                 withAnimation(.spring(response: 0.2, dampingFraction: 0.5)) {
                                     offset = .zero
